346e187bc87674cc35f89305dc854cf44e327e26
Evening Starlight <EveningStarlight@cmail.carleton.ca>
2023-04-14 19:46:35 +0000

    simple script
    doesn't run subfolders
    Needs direct read access on containers.

--- src/main.py
+++ src/main.py
@@ -1,35 +1,64 @@
-# Import Azure Resource Graph library
-import azure.mgmt.resourcegraph as arg
-
 # Import specific methods and models from other libraries
 from azure.mgmt.resource import SubscriptionClient
 from azure.identity import AzureCliCredential
 
-# Wrap all the work in a function
-def getresources( strQuery ):
-    # Get your credentials from Azure CLI (development only!) and get your subscription list
-    credential = AzureCliCredential()
-    subsClient = SubscriptionClient(credential)
-    subsRaw = []
-    for sub in subsClient.subscriptions.list():
-        subsRaw.append(sub.as_dict())
-    subsList = []
-    for sub in subsRaw:
-        subsList.append(sub.get('subscription_id'))
+import os, uuid
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
-    # Create Azure Resource Graph client and set options
-    argClient = arg.ResourceGraphClient(credential)
-    argQueryOptions = arg.models.QueryRequestOptions(result_format="objectArray")
+def main():
+    print("Starting Script")
+    print()
+    StorageAccount.getContainers(StorageAccount.list[0], True)
 
-    # Create query
-    argQuery = arg.models.QueryRequest(subscriptions=subsList, query=strQuery, options=argQueryOptions)
+class StorageAccount:
+    list = ['stndmfdidpb01sa']
 
-    # Run query
-    argResults = argClient.resources(argQuery)
+    def getContainers(storageAccount, verbose=False):
+        try:
+            print("Begin itterating over: " + storageAccount)
+            print()
 
-    # Show Python object
-    print(argResults)
+            account_url = "https://" + storageAccount + ".blob.core.windows.net"
+            credential = AzureCliCredential()
 
-# getresources("Resources | project name, type | limit 5 | order by name asc")
+            # Create the BlobServiceClient object
+            blob_service_client = BlobServiceClient(account_url, credential=credential)
 
-getresources("Resources | where type =~ 'Microsoft.Storage/storageAccounts' | where tags['ucid']=='DAaaS_Shared' | project name")
+            containerList = blob_service_client.list_containers()
+
+            for container in containerList:
+                containerObj= Container(account_url, container.name, credential)
+                if verbose:
+                    print(containerObj)
+
+            print('Run Successful')
+
+        except Exception as ex:
+            print('Exception:')
+            print(ex)
+
+class Container:
+    def __init__(self, account_url, name, credential):
+        container_client = ContainerClient(account_url, name, credential)
+        properties = container_client.get_container_properties()
+        blobs = container_client.list_blobs()
+        for blob in blobs:
+            print(blob.name + " : " + str(blob.blob_type))
+        totalSize = sum(blob['size'] for blob in blobs)
+
+        
+        
+        self.name = properties.name
+        self.ucid = properties.metadata["ucid"]
+        self.size = totalSize
+
+    def __str__(self):
+        string = "Container Name: " + self.name + "\n"
+        string = string + "UCID: " + self.ucid + "\n"
+        string = string + "Total Size: " + str(self.size) + "\n"
+
+        return string
+    
+
+if __name__ == "__main__":
+    main()
